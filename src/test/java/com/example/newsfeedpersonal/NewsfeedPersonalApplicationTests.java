9e9cbaa0e8b85fa582f8af1edc9c81fbceb0de26
Kim su-hwan <sshkim1013@naver.com>
2025-03-10 20:31:37 +0900

    first commit

---
+++ src/test/java/com/example/newsfeedpersonal/NewsfeedPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newsfeedpersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedPersonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
